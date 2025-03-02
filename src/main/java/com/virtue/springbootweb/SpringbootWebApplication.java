3637748bf384ab24634697377dd43bf4e76e8beb
악덕 <rudejrqkr97@gmail.com>
2025-03-02 20:22:20 +0900

    refactor: 클래스명 변경
    - 'SpringbootwebApplication' 클래스와 'SpringbootwebApplicationTests' 클래스의 이름을 각각 'SpringbootWebApplication'과 'SpringbootWebApplicationTests'로 변경

--- src/main/java/com/virtue/springbootweb/SpringbootWebApplication.java
+++ src/main/java/com/virtue/springbootweb/SpringbootWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootwebApplication {
+public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootwebApplication.class, args);
+		SpringApplication.run(SpringbootWebApplication.class, args);
 	}
 
 }
