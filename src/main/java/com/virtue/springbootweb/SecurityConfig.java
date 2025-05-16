b285b9bb1334f7da873dc7d1ce28719ae8085ac1
악덕 <rudejrqkr97@gmail.com>
2025-05-16 10:46:43 +0900

    feat: CSRF 토큰 저장소 추가
    - SecurityConfig.java 파일에 CSRF 토큰 저장소를 추가하였음

--- src/main/java/com/virtue/springbootweb/SecurityConfig.java
+++ src/main/java/com/virtue/springbootweb/SecurityConfig.java
@@ -7,21 +7,38 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository =
+                new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /* csrf 설정
+        http.csrf(csrf ->
+                csrf.csrfTokenRepository(csrfTokenRepository()).ignoringRequestMatchers("/login")
+        );
+        */
         http.csrf((csrf) -> csrf.disable());
         http.authorizeHttpRequests((authorize) ->
                 authorize.requestMatchers("/**").permitAll()
         );
+
         http.formLogin((formLogin)
                 -> formLogin.loginPage("/login")
                 .defaultSuccessUrl("/")
         );
+
         http.logout(logout -> logout.logoutUrl("/logout"));
         return http.build();
     }
